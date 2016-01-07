e7e6219afda4187e70a890b231ab1f68f78f765d
Ben Murray <ben@nextrealm.co.uk>
2016-01-07 21:16:31 +0000

    Change to pusher code.

--- server.js
+++ server.js
@@ -16,3 +16,25 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var bodyParser = require('body-parser');
+
+var Pusher = require('pusher');
+
+Pusher.app_id = '165033';
+Pusher.key = 'a21c1d876b090ae230df';
+Pusher.secret = '78ac97b8825d1adc0f48';
+
+var pusher = new Pusher({ appId: Pusher.app_id, key: Pusher.key, secret:  Pusher.secret });
+
+app.post('/pusher/auth', function(req, res) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
+
+pusher.trigger( 'chat_channel', 'message', { msg: "Welcome!" } );
