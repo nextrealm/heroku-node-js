21a043654126efc58e6453604461a808e186c702
Ben Murray <ben@nextrealm.co.uk>
2016-01-07 21:25:21 +0000

    Change to pusher code.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var Pusher = require('pusher');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,19 +18,6 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-var Pusher = require('pusher');
-
-/*Pusher.app_id = '165033';
-Pusher.key = 'a21c1d876b090ae230df';
-Pusher.secret = '78ac97b8825d1adc0f48';*/
-
 var pusher = new Pusher({ appId: APP_ID, key: APP_KEY, secret:  APP_SECRET });
 
 app.post('/pusher/auth', function(req, res) {
@@ -36,5 +26,15 @@ app.post('/pusher/auth', function(req, res) {
   var auth = pusher.authenticate(socketId, channel);
   res.send(auth);
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+/*Pusher.app_id = '165033';
+Pusher.key = 'a21c1d876b090ae230df';
+Pusher.secret = '78ac97b8825d1adc0f48';*/
+
+
 
 //pusher.trigger( 'chat_channel', 'message', { msg: "Welcome!" } );
