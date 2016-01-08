15580c23468610d63cc9b1f17f51825a1d373cf8
Ben Murray <ben@nextrealm.co.uk>
2016-01-08 00:14:18 +0000

    Change to pusher code.

--- server.js
+++ server.js
@@ -51,3 +51,9 @@ Pusher.secret = '78ac97b8825d1adc0f48';*/
 
 
 //pusher.trigger( 'chat_channel', 'message', { msg: "Welcome!" } );
+
+app.post('/api/', function(request, response) {
+	var input = request.body.input;
+
+	pusher.trigger( 'chat_channel', 'message', { msg: input.message } );
+});
